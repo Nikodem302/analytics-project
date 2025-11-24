734c0ace262751313355674ac352b6402670205a
Nikodem <nikodemnowak001@gmail.com>
2025-11-24 15:04:11 +0000

    Add startup file streamlit

---
+++ chapter11/streamlit/stramlit_football_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import logging 
+import pandas as pd
+
+if "base_url" not in st.session_state:
+    st.session_state["base_url"] = "https://legendary-xylophone-q74j4qrx64xr24v96-8000.app.github.dev/"
+
+logging.basicConfig(
+    filename="football_app.log",
+    level=logging.INFO,
+)
+st.set_page_config(page_title="Football App",
+                   page_icon=":material/sports_football:")
+
+page_1 = st.Page("page1.py", title="Team Rosters", icon=":material/trophy:")
+
+page_2 = st.Page("page2.py", title="Team Stats", icon=":material/star_border:")
+
+pg = st.navigation([page_1, page_2])
+pg.run()
